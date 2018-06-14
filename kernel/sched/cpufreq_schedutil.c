0f4bf067d0c406558e3008cad2ff1490272dd8c6
John Dias <joaodias@google.com>
2018-06-13 22:16:28 -0700

    cpufreq: schedutil: clear cached_raw_freq when invalidated
    The cpufreq_schedutil governor keeps a cache of the last
    raw frequency that was mapped to a supported device frequency.
    If the next request for a frequency matches the cached
    value, the policy's next_freq value is reused. But there
    are paths that can update the raw cached value without
    updating the next_freq value, and there are paths that
    can set the next_freq value without setting the raw
    cached value. On those paths, the cached value
    must be reset.
    
    The case that has been observed is when a frequency request
    reaches sugov_update_commit but is then rejected by to
    the sugov_up_down_rate_limit check.
    
    Bug: 116279565
    Change-Id: I7c585339a04ff1732054d6e5b36a57e2d41266aa
    Signed-off-by: John Dias <joaodias@google.com>
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -330,7 +330,9 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 
 	busy = use_pelt() && sugov_cpu_is_busy(sg_cpu);
 
-	if (flags & SCHED_CPUFREQ_DL) {
+	raw_spin_lock(&sg_policy->update_lock);
+
+	if (flags & SCHED_CPUFREQ_RT_DL) {
 		/* clear cache when it's bypassed */
 		sg_policy->cached_raw_freq = 0;
 		next_f = policy->cpuinfo.max_freq;
@@ -378,7 +380,7 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu, u64 time)
 			j_sg_cpu->iowait_boost_pending = false;
 			continue;
 		}
-		if (j_sg_cpu->flags & SCHED_CPUFREQ_DL) {
+		if (j_sg_cpu->flags & SCHED_CPUFREQ_RT_DL) {
 			/* clear cache when it's bypassed */
 			sg_policy->cached_raw_freq = 0;
 			return policy->cpuinfo.max_freq;
@@ -422,7 +424,7 @@ static void sugov_update_shared(struct update_util_data *hook, u64 time,
 	sg_cpu->last_update = time;
 	if (sugov_should_update_freq(sg_policy, time) &&
 		!(flags & SCHED_CPUFREQ_CONTINUE)) {
-		if (flags & SCHED_CPUFREQ_DL) {
+		if (flags & SCHED_CPUFREQ_RT_DL) {
 			next_f = sg_policy->policy->cpuinfo.max_freq;
 			/* clear cache when it's bypassed */
 			sg_policy->cached_raw_freq = 0;
