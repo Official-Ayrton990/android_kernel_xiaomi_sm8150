d20b158099d243a4f62279882ade046ed54d2fcc
Sultan Alsawaf <sultan@kerneltoast.com>
2018-12-09 17:45:01 -0800

    drm/atomic: Boost CPU and DDR bus when committing a new frame
    In order to reduce jank, request CPU and DDR bus boosts whenever a new frame
    is ready to be rendered to the display. The boosts should be sufficient
    enough to render 60 FPS without any dropped frames when there is no
    significant external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -31,6 +31,7 @@
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
 #include <linux/sync_file.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include "drm_crtc_internal.h"
@@ -2251,6 +2252,11 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+	}
+
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 	}
 
 	drm_modeset_acquire_init(&ctx, 0);
