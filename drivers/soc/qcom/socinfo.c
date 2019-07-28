018feb97b32ded2a13bcf1fdd2c5edb7346b9f28
Demon000 <demonsingur@gmail.com>
2019-07-28 07:52:50 +0200

    socinfo: add support for cepheus

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -70,6 +70,7 @@ enum {
 	HW_PLATFORM_HDK = 31,
 	HW_PLATFORM_IOT = 32,
 	HW_PLATFORM_IDP = 34,
+       HW_PLATFORM_F1  = 37,
 	HW_PLATFORM_INVALID
 };
 
@@ -93,7 +94,8 @@ const char *hw_platform[] = {
 	[HW_PLATFORM_ADP] = "ADP",
 	[HW_PLATFORM_HDK] = "HDK",
 	[HW_PLATFORM_IOT] = "IOT",
-	[HW_PLATFORM_IDP] = "IDP"
+	[HW_PLATFORM_IDP] = "IDP",
+	[HW_PLATFORM_F1]  = "CEPHEUS",
 };
 
 enum {
@@ -1760,6 +1762,16 @@ static void socinfo_select_format(void)
 		socinfo_format = socinfo->v0_1.format;
 	}
 }
+
+uint32_t get_hw_version_platform(void)
+{
+	uint32_t hw_type = socinfo_get_platform_type();
+	if (hw_type == HW_PLATFORM_F1)
+		return HARDWARE_PLATFORM_CEPHEUS;
+	else
+		return HARDWARE_PLATFORM_UNKNOWN;
+}
+EXPORT_SYMBOL(get_hw_version_platform);
 
 int __init socinfo_init(void)
 {
