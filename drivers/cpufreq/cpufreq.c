7fdf4abb2198335810fbfc7a203c02c31524e969
Danny Lin <danny@kdrag0n.dev>
2019-08-04 07:39:15 +0000

    cpufreq: Only block min freq setting for booster processes
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -727,7 +727,8 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (&policy->object == &policy->min)				\
+	if (&policy->object == &policy->min &&				\
+	    task_is_booster(current))					\
 		return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
