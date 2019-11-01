68043561bc178fe906c0019052c7c1f1a5f985cc
flar2 <asegaert@gmail.com>
2019-11-01 17:13:45 -0400

    KLAPSE: move set_rgb_slider to proper place

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -24,10 +24,6 @@
 #include "dsi_ctrl_hw.h"
 #include "dsi_parser.h"
 
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
-#endif
-
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -1249,10 +1245,6 @@ static int dsi_panel_parse_misc_host_config(struct dsi_host_common_cfg *host,
 		pr_debug("[%s] t_clk_post = %d\n", name, val);
 	}
 	
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
-
 	val = 0;
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-t-clk-pre", &val);
 	if (!rc) {
