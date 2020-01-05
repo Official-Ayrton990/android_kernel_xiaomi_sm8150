b9ba167cfa09527fd5978c16ccafefcf3267ef0b
Akash <akashniki@gmail.com>
2020-01-05 16:23:34 +0530

    input:touchscreen: Change DT2W keycode
    Other roms don't pick this commit. Its only needed for Paranoid Android

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -3545,7 +3545,9 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 
 		switch (event[2]) {
 		case GEST_ID_DBLTAP:
-			value = KEY_WAKEUP;
+			if (!info->gesture_enabled)
+				goto gesture_done;
+			value = KEY_DOUBLE_TAP;
 			logError(0, "%s %s: double tap ! \n", tag, __func__);
 			get_coords = 0;
 			break;
@@ -5871,7 +5873,7 @@ static int fts_probe(struct spi_device *client)
 			     DISTANCE_MAX, 0, 0);
 
 #ifdef GESTURE_MODE
-	input_set_capability(info->input_dev, EV_KEY, KEY_WAKEUP);
+	input_set_capability(info->input_dev, EV_KEY, KEY_DOUBLE_TAP);
 
 	input_set_capability(info->input_dev, EV_KEY, KEY_M);
 	input_set_capability(info->input_dev, EV_KEY, KEY_O);
