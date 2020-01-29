997b8a69316f414263cd50599eae069525b4d685
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-01-29 05:23:27 -0500

    devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting
    This will enable us to more accurately replicate Pixel 4 userspace
    DDR boosting by adding support for both CPUBW and LLCCBW devices.
    
    https://android.googlesource.com/device/google/coral/+/refs/heads/master/init.power.rc
    https://android.googlesource.com/device/google/coral/+/refs/heads/master/powerhint.json
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2246,6 +2246,11 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
+		
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
