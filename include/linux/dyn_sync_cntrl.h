c5bb675a891a828f79563fdfdf99d5964884f9e5
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-02-05 13:32:01 +0100

    Fs: Add dynamic fsync
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

---
+++ include/linux/dyn_sync_cntrl.h
@@ -0,0 +1,16 @@
+/* 
+ * Dynamic sync control driver definitions (V2)
+ * 
+ * by andip71 (alias Lord Boeffla)
+ * 
+ * sdm845 changes by pappschlumpf (Erik Müller)
+ */
+
+#define DYN_FSYNC_ACTIVE_DEFAULT true
+#define DYN_FSYNC_VERSION_MAJOR 2
+#define DYN_FSYNC_VERSION_MINOR 2
+
+extern bool suspend_active;
+extern bool dyn_fsync_active;
+
+
