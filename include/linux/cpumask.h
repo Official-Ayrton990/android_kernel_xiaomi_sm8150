b7e2ad5fab05114c86734f7fd382c9faa236ea0b
Ayrton990 <carlos.ayrton.la@gmail.com>
2020-03-27 01:46:26 -0600

    cpumask: Add cpumask for the silver CPU cluster

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -92,6 +92,7 @@ extern struct cpumask __cpu_online_mask;
 extern struct cpumask __cpu_present_mask;
 extern struct cpumask __cpu_active_mask;
 extern struct cpumask __cpu_isolated_mask;
+extern const struct cpumask *const cpu_lp_mask;
 #define cpu_possible_mask ((const struct cpumask *)&__cpu_possible_mask)
 #define cpu_online_mask   ((const struct cpumask *)&__cpu_online_mask)
 #define cpu_present_mask  ((const struct cpumask *)&__cpu_present_mask)
