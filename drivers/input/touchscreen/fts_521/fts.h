9f50f3ed7f53f960eeda5bf307007867820c527e
Demon000 <demonsingur@gmail.com>
2020-04-21 12:18:40 +0300

    fts_521: fix disabling touch count dumping

--- drivers/input/touchscreen/fts_521/fts.h
+++ drivers/input/touchscreen/fts_521/fts.h
@@ -331,11 +331,9 @@ struct fts_ts_info {
 #ifdef CONFIG_TOUCHSCREEN_ST_DEBUG_FS
 	struct dentry *debugfs;
 #endif
-#ifdef CONFIG_FTS_TOUCH_COUNT_DUMP
 	struct class *fts_tp_class;
 	struct device *fts_touch_dev;
 	char *current_clicknum_file;
-#endif
 #ifdef CONFIG_SECURE_TOUCH
 	struct fts_secure_info *secure_info;
 #endif
