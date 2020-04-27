32fe564f7c12fbb4ad7c27422030248903dff9e6
Ayrton990 <15030201@itcelaya.edu.mx>
2020-04-27 14:50:22 -0500

    Revert "cpu-boost: Reset Dynamic SchedTune Boost only if it is currently active"
    This reverts commit 491e7189b711a7ed658d112c2cee0c5d1b10dc24.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -46,7 +46,6 @@ static bool sched_boost_active;
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 static int dynamic_stune_boost;
 module_param(dynamic_stune_boost, uint, 0644);
-static bool stune_boost_active;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 static struct delayed_work input_boost_rem;
@@ -183,10 +182,7 @@ static void do_input_boost_rem(struct work_struct *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
-	if (stune_boost_active) {
-		reset_stune_boost("top-app");
-		stune_boost_active = false;
-	}
+	reset_stune_boost("top-app");
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	/* Update policies for all online CPUs */
@@ -211,11 +207,6 @@ static void do_input_boost(struct work_struct *work)
 		sched_boost_active = false;
 	}
 
-	if (stune_boost_active) {
-		reset_stune_boost("top-app");
-		stune_boost_active = false;
-	}
-
 	/* Set the input_boost_min for all CPUs in the system */
 	pr_debug("Setting input boost min for all CPUs\n");
 	for_each_possible_cpu(i) {
@@ -228,9 +219,7 @@ static void do_input_boost(struct work_struct *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Set dynamic stune boost value */
-	ret = do_stune_boost("top-app", dynamic_stune_boost);
-	if (!ret)
-		stune_boost_active = true;
+	do_stune_boost("top-app", dynamic_stune_boost);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
