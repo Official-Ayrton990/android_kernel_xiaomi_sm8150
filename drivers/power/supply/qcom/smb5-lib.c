65fa99eeb04a8da7e4773555fe404d07842c0ea5
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-08-03 16:19:51 +0000

    Revert "power: supply: Add a parameter for skipping thermal mitigation"
    This reverts commit 917b9107a52adace50784a0abe34f3c54a3eef34.

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -22,7 +22,6 @@
 #include <linux/pmic-voter.h>
 #include <linux/of_batterydata.h>
 #include <linux/ktime.h>
-#include <linux/module.h>
 #include "smb5-lib.h"
 #include "smb5-reg.h"
 #include "schgm-flash.h"
@@ -2624,9 +2623,6 @@ int smblib_set_prop_batt_status(struct smb_charger *chg,
 #define ADAPTER_ZIMI_CAR_POWER    0x0b
 
 #ifdef CONFIG_THERMAL
-unsigned int skip_therm = 0;
-module_param(skip_therm, uint, S_IWUSR | S_IRUGO);
-
 static int smblib_dc_therm_charging(struct smb_charger *chg,
 					int temp_level)
 {
@@ -2647,11 +2643,6 @@ static int smblib_dc_therm_charging(struct smb_charger *chg,
 	rc = power_supply_get_property(chg->wls_psy,
 				POWER_SUPPLY_PROP_WIRELESS_VERSION,
 				&val);
-	if (skip_therm) {
-		vote(chg->dc_icl_votable, THERMAL_DAEMON_VOTER, false, 0);
-		return 0;
-	}
-
 		switch (pval.intval) {
 		case ADAPTER_XIAOMI_QC3:
 		case ADAPTER_ZIMI_CAR_POWER:
@@ -2747,12 +2738,6 @@ static int smblib_therm_charging(struct smb_charger *chg)
 
 	if (chg->system_temp_level >= MAX_TEMP_LEVEL)
 		return 0;
-
-	if (skip_therm) {
-		vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, false, 0);
-		vote(chg->usb_icl_votable, THERMAL_DAEMON_VOTER, false, 0);
-		return 0;
-	}
 
 	switch (chg->real_charger_type) {
 	case POWER_SUPPLY_TYPE_USB_HVDCP:
