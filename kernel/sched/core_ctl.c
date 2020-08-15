6b08d5949256a4171b7922883a6b53e79f0fde4b
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-08-15 11:36:37 -0500

    core_ctl: block init options
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -1200,7 +1200,7 @@ static int core_ctl_isolation_dead_cpu(unsigned int cpu)
 }
 
 /* ============================ init code ============================== */
-
+#if 0
 static struct cluster_data *find_cluster_by_first_cpu(unsigned int first_cpu)
 {
 	unsigned int i;
@@ -1306,3 +1306,4 @@ static int __init core_ctl_init(void)
 }
 
 late_initcall(core_ctl_init);
+#endif
