e3ea6794b20fb886279e797fa008dc4a84aac7d8
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-09-10 00:04:46 -0500

    Revert "scripts/dtc: Update to upstream version v1.4.6-9-gaadd0b65c987"
    This reverts commit f6f74074e799484de1a2c6c5f18909a022157cee.
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- scripts/dtc/livetree.c
+++ scripts/dtc/livetree.c
@@ -216,7 +216,7 @@ struct node *merge_nodes(struct node *old_node, struct node *new_node)
 	return old_node;
 }
 
-struct node * add_orphan_node(struct node *dt, struct node *new_node, char *ref)
+void add_orphan_node(struct node *dt, struct node *new_node, char *ref)
 {
 	static unsigned int next_orphan_fragment = 0;
 	struct node *node;
@@ -236,7 +236,6 @@ struct node * add_orphan_node(struct node *dt, struct node *new_node, char *ref)
 	name_node(node, name);
 
 	add_child(dt, node);
-	return dt;
 }
 
 struct node *chain_node(struct node *first, struct node *list)
@@ -508,7 +507,7 @@ struct node *get_node_by_path(struct node *tree, const char *path)
 
 	for_each_child(tree, child) {
 		if (p && (strlen(child->name) == p-path) &&
-		    strprefixeq(path, p - path, child->name))
+				strneq(path, child->name, p-path))
 			return get_node_by_path(child, p+1);
 		else if (!p && streq(path, child->name))
 			return child;
@@ -541,10 +540,7 @@ struct node *get_node_by_phandle(struct node *tree, cell_t phandle)
 {
 	struct node *child, *node;
 
-	if ((phandle == 0) || (phandle == -1)) {
-		assert(generate_fixups);
-		return NULL;
-	}
+	assert((phandle != 0) && (phandle != -1));
 
 	if (tree->phandle == phandle) {
 		if (tree->deleted)
